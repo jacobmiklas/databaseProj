92770e336f9d640e904fff1507c6bfd0fab90efe
jacobmiklas <jmiklas23@gmail.com>
2025-04-08 14:33:45 -0600

    added sql file

---
+++ soccerDatabase.sql
@@ -0,0 +1,13 @@
+-- init.sql
+
+CREATE DATABASE IF NOT EXISTS soccerdb;
+
+USE soccerdb;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM soccerdb.users;
